b44f54185cff79660e26b49b2204a0620b59c3a7
Brian Schmeling <Bschmeling24@gmail.com>
2019-11-26 13:39:15 -0600

    Add drawing function

--- EASscript.js
+++ EASscript.js
@@ -3,10 +3,19 @@ const container = document.querySelector('#container');
 const grid = document.createElement('div');
 grid.classList.add('grid');
 
-    for (let i = 0; i < 256; i++) {
-        let cell = document.createElement('div');
-        cell.classList.add('cell');
-        grid.appendChild(cell);
-    }
+const gridSize = 16;
+
+//Create 16x16 grid
+for (let i = 0; i < gridSize*gridSize ; i++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.addEventListener('mouseenter', function(e){changeColor(e)});
+    grid.appendChild(cell);
+}
 
 container.appendChild(grid);
+
+
+function changeColor(event, mode){
+    event.currentTarget.style.backgroundColor='black';
+}
