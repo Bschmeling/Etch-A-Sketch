dd992b61a798ffbd4cec3265df12f2308b3243bd
Brian Schmeling <Bschmeling24@gmail.com>
2019-11-27 17:10:52 -0600

    Update functions to change ammount of boxes

--- EASscript.js
+++ EASscript.js
@@ -3,19 +3,34 @@ const container = document.querySelector('#container');
 const grid = document.createElement('div');
 grid.classList.add('grid');
 
-const gridSize = 16;
-
 //Create 16x16 grid
-for (let i = 0; i < gridSize*gridSize ; i++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.addEventListener('mouseenter', function(e){changeColor(e)});
-    grid.appendChild(cell);
+function newGrid(gridSize){
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+    for (let i = 0; i < gridSize*gridSize ; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('mouseenter', function(e){changeColor(e)});
+        grid.appendChild(cell);
+    }
 }
 
 container.appendChild(grid);
 
-
-function changeColor(event, mode){
+//Change color to black on mouseover
+function changeColor(event){
     event.currentTarget.style.backgroundColor='black';
 }
+
+function reset() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.remove();
+    });
+    const userChoice = prompt("how many squares per side?");
+    newGrid(userChoice);
+    
+}
+
+
+newGrid(16);
